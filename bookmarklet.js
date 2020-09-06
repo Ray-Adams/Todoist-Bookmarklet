15a6eef8377c7223397a7386562d84e5676c60d9
Ray Adams <63924749+Ray-Adams@users.noreply.github.com>
2020-09-06 15:18:24 +0000

    Update and rename classic.js to bookmarklet.js

---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:window.open('https://todoist.com',null,'width=480,height=550').focus()
--- classic.js
+++
@@ -1,1 +0,0 @@
-javascript:window.open("https://todoist.com",null,"width=480, height=550").focus();
